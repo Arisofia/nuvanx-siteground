ed0ef45484eedbf8d521308f5fbef3af4ec936de
Jeninefer Deras <jenineferderas@hotmail.com>
2026-07-18 09:23:21 +0200

    fix(blog): add styled search results template

---
+++ wp-content/themes/nuvanx-medical/search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Journal search results.
+ *
+ * @package nuvanx-medical
+ */
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+<main id="nvx-main" class="nvx-main nvx-blog-index" role="main">
+	<?php get_template_part( 'template-parts/content/nvx-blog-archive' ); ?>
+</main>
+<?php
+get_footer();

32485f3482c602cb6bff8d378e19762252f8d47a
Your Name <you@example.com>
2026-07-23 10:47:18 +0200

    chore: remove temporary fix scripts and hygiene report

--- fix_syntax.py
+++
@@ -1,7 +0,0 @@
-with open("wp-content/themes/nuvanx-medical/inc/nvx-structured-data.php", "r") as f:
-    content = f.read()
-
-content = content.replace("function nvx_endolift_price_from_eur() { return 0.0; }\n\n\treturn (float) $catalog['endolift']['ojeras']['pvp'];\n}", "function nvx_endolift_price_from_eur() { return 0.0; }")
-
-with open("wp-content/themes/nuvanx-medical/inc/nvx-structured-data.php", "w") as f:
-    f.write(content)

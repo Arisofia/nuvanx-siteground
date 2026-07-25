f40082693b2eab17a52787f033feb0333c4fef12
Jeninefer Deras <jenineferderas@hotmail.com>
2026-07-25 16:21:31 +0200

    refactor(valoracion): make landing fully GitHub-owned

--- wp-content/themes/nuvanx-medical/templates/page-landing-valoracion.php
+++ wp-content/themes/nuvanx-medical/templates/page-landing-valoracion.php
@@ -9,6 +9,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require_once get_template_directory() . '/inc/nvx-clinics-valoracion-coherence.php';
+ob_start();
+get_template_part( 'template-parts/content/nvx-valoracion-github' );
+$nvx_valoracion_content = ob_get_clean();
 
+set_query_var( 'nvx_shell_content', $nvx_valoracion_content );
+set_query_var( 'nvx_shell_skip_header', true );
+set_query_var( 'nvx_shell_no_wrapper', true );
 get_template_part( 'template-parts/content/nvx-page-shell' );

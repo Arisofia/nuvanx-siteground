300f02a0d068342e32d2c41ac9a5a5d7692c2d6a
Jeninefer Deras <jenineferderas@hotmail.com>
2026-07-25 16:43:18 +0200

    fix(valoracion): resolve managed page id deterministically

--- wp-content/themes/nuvanx-medical/templates/page-landing-valoracion.php
+++ wp-content/themes/nuvanx-medical/templates/page-landing-valoracion.php
@@ -10,7 +10,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once get_template_directory() . '/inc/nvx-github-managed-page-state.php';
-nvxSyncGithubManagedPageState( (int) get_queried_object_id(), 'valoracion' );
+
+$page_id = (int) get_queried_object_id();
+if ( $page_id < 1 ) {
+	$page_id = (int) get_the_ID();
+}
+if ( $page_id < 1 ) {
+	$page = get_page_by_path( 'madrid/valoracion', OBJECT, 'page' );
+	if ( ! $page instanceof WP_Post ) {
+		$page = get_page_by_path( 'valoracion', OBJECT, 'page' );
+	}
+	$page_id = $page instanceof WP_Post ? (int) $page->ID : 0;
+}
+nvxSyncGithubManagedPageState( $page_id, 'valoracion' );
 
 ob_start();
 get_template_part( 'template-parts/content/nvx-valoracion-github' );

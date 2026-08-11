c3c647e57d14cd05cccec3d8e9b9c404d93f9b60
Jeninefer Deras <jenineferderas@hotmail.com>
2026-08-11 06:07:46 +0200

    fix(sonar): align clinics navigation conventions

--- wp-content/themes/nuvanx-medical/inc/nvx-clinics-hub-navigation.php
+++ wp-content/themes/nuvanx-medical/inc/nvx-clinics-hub-navigation.php
@@ -11,7 +11,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+    exit;
 }
 
 /**
@@ -20,31 +20,31 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @param string $content Rendered page content.
  * @return string
  */
-function nvx_clinics_hub_restore_jump_navigation( string $content ): string {
-	if ( is_admin() || ! function_exists( 'nvxIsClinicsHub' ) || ! nvxIsClinicsHub() || false === strpos( $content, 'nvx-clinics-nav' ) ) {
-		return $content;
-	}
+function nvxClinicsHubRestoreJumpNavigation( string $content ): string {
+    if ( is_admin() || ! function_exists( 'nvxIsClinicsHub' ) || ! nvxIsClinicsHub() || false === strpos( $content, 'nvx-clinics-nav' ) ) {
+        return $content;
+    }
 
-	return (string) preg_replace_callback(
-		'/<nav\b[^>]*class=["\'][^"\']*\bnvx-clinics-nav\b[^"\']*["\'][^>]*>[\s\S]*?<\/nav>/iu',
-		static function ( array $matches ): string {
-			$nav = (string) $matches[0];
-			$nav = (string) preg_replace(
-				'/(<a\b[^>]*\bhref=)["\'][^"\']*["\']([^>]*>\s*Chamber[ií]\s*<\/a>)/iu',
-				'$1"#clinica-chamberi"$2',
-				$nav,
-				1
-			);
-			$nav = (string) preg_replace(
-				'/(<a\b[^>]*\bhref=)["\'][^"\']*["\']([^>]*>\s*Salamanca[–-]Goya\s*<\/a>)/iu',
-				'$1"#clinica-goya"$2',
-				$nav,
-				1
-			);
-			return $nav;
-		},
-		$content,
-		1
-	);
+    return (string) preg_replace_callback(
+        '/<nav\b[^>]*class=["\'][^"\']*\bnvx-clinics-nav\b[^"\']*["\'][^>]*>[\s\S]*?<\/nav>/iu',
+        static function ( array $matches ): string {
+            $nav = (string) $matches[0];
+            $nav = (string) preg_replace(
+                '/(<a\b[^>]*\bhref=)["\'][^"\']*["\']([^>]*>\s*Chamber[ií]\s*<\/a>)/iu',
+                '$1"#clinica-chamberi"$2',
+                $nav,
+                1
+            );
+            $nav = (string) preg_replace(
+                '/(<a\b[^>]*\bhref=)["\'][^"\']*["\']([^>]*>\s*Salamanca[–-]Goya\s*<\/a>)/iu',
+                '$1"#clinica-goya"$2',
+                $nav,
+                1
+            );
+            return $nav;
+        },
+        $content,
+        1
+    );
 }
-add_filter( 'the_content', 'nvx_clinics_hub_restore_jump_navigation', 31 );
+add_filter( 'the_content', 'nvxClinicsHubRestoreJumpNavigation', 31 );
